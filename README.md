4a169c8723f20b6532f22c4afee4754db162310b
Claire Lipskey <claire.lipskey@gmail.com>
2021-08-01 21:48:12 -0500

    Adds readme; detachMultiple

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shortcode
+
+This is a fun little project I started when I was thinking of a jQuery-esque library, but targeting ES6+/ESNext and written in TypeScript. This is an attampt to make it in public, so enjoy.
--- dom/detach.ts
+++ dom/detach.ts
@@ -18,9 +18,25 @@ export const detach = (element: HTMLElement | string, identifier: string) => {
     }
 }
 
+export const detachMultiple = (selector: string, identifier: string) => {
+    let candidates = document.querySelectorAll(selector);
+    if (candidates) {
+        let detachables = Array.from(candidates).map(item => {
+            return { element: item, parent: item.parentElement }
+        })
+
+        addToStorage({ name: identifier, value: detachables })
+        candidates.forEach(el => el.remove())
+    }
+}
+
 export const reAttach = (identifier: string) => {
     let el = searchStorage(identifier) as DetachedItem;
     if (el) {
-        el.parent.append(el.element);
+        if (el instanceof Array) {
+            el.forEach((attachable: DetachedItem) => attachable.parent.append(attachable.element))
+        } else {
+            el.parent.append(el.element);
+        }
     }
 }
