06e26815b948695953a54e580c861e01cd601985
Claire Lipskey <clairebaire@users.noreply.github.com>
2021-08-11 12:20:26 -0500

    Test from Codespaces

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { forms } from './forms/index';
 import { dom } from './dom/index';
 import { shortcode } from './utils/construct';
 
+// Test commit from Github Codespaces
+
 const scripts = {
     dom,
     forms
